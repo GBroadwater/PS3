acffb7c1fb52ec23e8725fc279f12edf75b3335c
geoff <geoff@128.4.123.8>
2017-10-10 14:01:39 -0400

    Tests are slightly more efficient

--- PS3/src/test/java/pkgCore/DeckTest.java
+++ PS3/src/test/java/pkgCore/DeckTest.java
@@ -9,36 +9,33 @@ import pkgEnum.eSuit;
 import pkgException.DeckException;
 
 public class DeckTest {
+	
+	Deck d = new Deck();
 
 	@Test(expected = DeckException.class)
 	public void TestEmptyDeck() throws DeckException {
-		Deck d = new Deck();
 		for (int i = 0; i < 60; i++)
 			d.Draw();
 	}
 
 	@Test
 	public void TestDrawSuit() {
-		Deck d = new Deck();
 		assertEquals(eSuit.CLUBS, d.Draw(eSuit.CLUBS).geteSuit());
 
 	}
 
 	@Test
 	public void TestDrawRank() {
-		Deck d = new Deck();
 		assertEquals(eRank.NINE, d.Draw(eRank.NINE).geteRank());
 	}
 
 	@Test
 	public void TestDeckRankCount() {
-		Deck d = new Deck();
 		assertEquals(4, d.DeckRankCount(eRank.JACK));
 	}
 
 	@Test
 	public void TestDeckSuitCount() {
-		Deck d = new Deck();
 		assertEquals(13, d.DeckSuitCount(eSuit.DIAMONDS));
 	}
 
